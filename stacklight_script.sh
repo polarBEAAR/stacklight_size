d2ea2c826c5594ed1f2f02aff37a320a9c95c594
Maggie <47176987+polarBEAAR@users.noreply.github.com>
2022-01-11 19:13:17 +0400

    Update stacklight_script.sh

--- stacklight_script.sh
+++ stacklight_script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export KUBECONFIG=/Users/mmazepa/kubeconfig-child
+key_path=<keypath>
 exec 2> error.log
 
 node_list=$(kubectl get node -l=stacklight -o custom-columns=NAME:.status.addresses[1].address,IP:.status.addresses[0].address | grep -v IP)
@@ -12,7 +13,7 @@ do
 	printf "%-51s %-16s\n" "$node_name" "$node_ip"
 	for vol_path in $(kubectl get pv -o custom-columns=NAME:.metadata.name,STATUS:.status.phase,PATH:.spec.local.path,NODE:.spec.nodeAffinity.required.nodeSelectorTerms[0].matchExpressions[0].values[0] | grep -i bound | grep "$node_name" | grep "stacklight" | awk '{print $3}' | awk -F '/vol' '{print $1}')
 		do
-		size=$(ssh -q -i /Users/mmazepa/ssh_keys/eu-production-child mcc-user@"$node_ip" "du -d 0 -h "$vol_path"" | awk '{print $1}')
+		size=$(ssh -q -i "$keypath" mcc-user@"$node_ip" "du -d 0 -h "$vol_path"" | awk '{print $1}')
 		printf "%-68s %-50s %-10s\n" " " "$vol_path" "$size"
 	done
 done
